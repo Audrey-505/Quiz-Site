8fc022205fcb37713051a8dfb645577587b52a84
Audrey Alcantara <adrianaquin22@gmail.com>
2022-11-13 17:28:01 -0500

    questions display code started

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-alert("hello")
+// this code grabs the elements needed 
+var startBtn = document.getElementById("quiz-button")
+var quizQuestions = document.getElementById("quiz-questions")
+var quizAnswers = document.getElementById("quiz-answers")
+var quizResults = document.getElementById("quiz-results")
+var quizScore = document.getElementById("quiz-score")
+var quizTimer = document.getElementById("quiz-timer")
+
+// this code holds the array of questions that will be asked
+questionsArray = ['Is Javascript the same as Java?', 'Inside which HTML element do we put the JavaScript?', 'What is the correct syntax for referring to an external script called "xxx.js"?', 'The external JavaScript file must contain the <script> tag.', 'How do you write "Hello World" in an alert box?'];
+
+
+// this code will run once start button is clicked
+function beginTimer() {
+
+}
+
+
+// this code should be invoked once the timer starts 
+for(i = 0; i < questionsArray.length; i++){
+        quizQuestions.innerHTML = questionsArray[0]
+    }
+

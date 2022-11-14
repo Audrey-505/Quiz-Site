a3b5a17fdb5f53cb694e0de7d5fb2b4d58a282e8
Audrey Alcantara <adrianaquin22@gmail.com>
2022-11-13 21:30:22 -0500

    error with object

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 // this code grabs the elements needed 
 var startBtn = document.getElementById("quiz-button")
 var quizQuestions = document.getElementById("quiz-questions")
-var quizAnswers = document.getElementById("quiz-answers")
+var quizAnswer = document.getElementById("quiz-answer")
+var optionOne = document.getElementById("op1")
+var optionTwo = document.getElementById("op2")
+var optionThree = document.getElementById("op3")
+var optionFour = document.getElementById("op4")
 var quizResults = document.getElementById("quiz-results")
 var quizScore = document.getElementById("quiz-score")
 var quizTimer = document.getElementById("quiz-timer")
 
 // this code holds the array of questions that will be asked
-questionsArray = ['Is Javascript the same as Java?', 'Inside which HTML element do we put the JavaScript?', 'What is the correct syntax for referring to an external script called "xxx.js"?', 'The external JavaScript file must contain the <script> tag.', 'How do you write "Hello World" in an alert box?'];
+var questionHolder = [
+    {
+        question: 'Is Javascript the same as Java?',
+        choices: ['a. Yes', 'b. No'],
+        answer: 1
+    },
+    {
+        question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+        choices: ['a. <script name="xxx.js">','b. <script href="xxx.js">','c. <script src="xxx.js">'],
+        answer: 2
+    },
+    {
+        question: 'Inside which HTML element do we put the JavaScript?',
+        choices: ['<javascript>', '<script>', '<scripting>', '<js>'],
+        answer: 1
+    },
+    {
+        question: 'The external JavaScript file must contain the <script> tag.',
+        choices: ['True', 'False'],
+        answer: 1
+    }
+]
 
 
 // this code will run once start button is clicked
@@ -16,8 +41,11 @@ function beginTimer() {
 }
 
 
-// this code should be invoked once the timer starts 
-for(i = 0; i < questionsArray.length; i++){
-        quizQuestions.innerHTML = questionsArray[0]
-    }
+// this code should be invoked once the timer starts
+questionHolder.forEach(beginQuiz)
 
+function beginQuiz(){
+    quizQuestions.innerHTML = questionHolder[0].question
+    //quizAnswer.innerHTML = questionHolder[0].choices.join(' ')
+    optionOne.innerHTML = questionHolder.question[0].choices[0].join(' ')
+}
